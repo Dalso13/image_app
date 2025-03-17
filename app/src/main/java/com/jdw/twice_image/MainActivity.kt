cf3ef1241dda74a755d05e7a4fad51d79e4a4dcf
Dalso13 <flskwhdhk@gmail.com>
2025-03-17 09:44:22 +0900

    feat: list , foreach 로 클릭 이벤트 구현 간소화

--- app/src/main/java/com/jdw/twice_image/MainActivity.kt
+++ app/src/main/java/com/jdw/twice_image/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.jdw.twice_image
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bths = listOf<ImageView>(
+            findViewById(R.id.twice_1),
+            findViewById(R.id.twice_2),
+            findViewById(R.id.twice_3),
+            findViewById(R.id.twice_4),
+            findViewById(R.id.twice_5),
+            findViewById(R.id.twice_6),
+            findViewById(R.id.twice_7),
+            findViewById(R.id.twice_8),
+            findViewById(R.id.twice_9),
+        )
+
+        bths.forEach { btn : ImageView ->
+            btn.setOnClickListener {
+                val intent = Intent(this, ImageInsideActivity::class.java)
+
+                startActivity(intent)
+            }
+        }
 
     }
 }
